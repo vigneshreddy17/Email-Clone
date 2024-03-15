78bfeca89e2ffc255198e1b53a9f902089f19485
vigneshreddy17 <harry.chinnu09@gmail.com>
2024-03-15 14:27:13 -0700

    Added Logout and option select API along with unit test.

--- src/main/java/com/projects/emailclone/Controller/EmailController.java
+++ src/main/java/com/projects/emailclone/Controller/EmailController.java
@@ -2,20 +2,23 @@ package com.projects.emailclone.Controller;
 
 import com.projects.emailclone.Service.UserImplementation;
 import com.projects.emailclone.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.projects.emailclone.Model.User;
+
+import java.util.Scanner;
 
 
 @RestController
 public class EmailController {
     public UserService userService;
     public UserImplementation userImplementation;
+    final boolean isLoggedIn = false;
+    HttpSession userSession = null;
 
     @Autowired
     public EmailController(UserService userService, UserImplementation userImplementation) {
@@ -37,16 +40,62 @@ public class EmailController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestParam("firstName") String firstName, @RequestParam("password") String password) {
+    public String loginUser(@RequestParam("firstName") String firstName, @RequestParam("password") String password,
+                HttpServletRequest request) {
         final boolean result = userImplementation.checkIfUserExists(firstName);
         if (result) {
             final boolean passwordChecker = userImplementation.checkPassword(firstName, password);
-            if (passwordChecker)
-                return ResponseEntity.status(HttpStatus.OK).body("Login Successfully");
+            if (passwordChecker) {
+                userSession = request.getSession(true);
+                userSession.setAttribute("firstName", firstName);
+                return showOptions(userSession);
+            }
             else
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect Password");
+                return "Incorrect Password!!";
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User Not found");
+        return "User not found";
     }
+
+    @GetMapping("/show-options")
+    public String showOptions(final HttpSession session) {
+        String firstNameFromSession = (String) session.getAttribute("firstName");
+        return "Login Successful, Welcome " + firstNameFromSession
+                +"\n1) Show Messages\n2) Send Messages\n3) Log-out";
+    }
+
+    @PostMapping("/selected-option")
+    public ResponseEntity<String> directToSelectedOption(@RequestParam("option") int option) {
+        try {
+            if (!userSession.isNew() && userSession != null) {
+                return switch (option) {
+                    case 1 -> ResponseEntity.status(HttpStatus.OK).body("Show Messages");
+                    case 2 -> ResponseEntity.status(HttpStatus.OK).body("Send Messages");
+                    case 3 -> ResponseEntity.status(HttpStatus.OK).body("Log out");
+                    default -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Option Selected");
+                };
+            }
+        }
+        catch (Exception exception) {
+            ResponseEntity.status(HttpStatus.CONFLICT).body("Invalid Option Selected");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Option Selected");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout() {
+        try {
+            if (!userSession.isNew()) {
+                String firstName = (String) userSession.getAttribute("firstName");
+                userSession.invalidate();
+                return ResponseEntity.status(HttpStatus.OK).body("Logged out successfully, Bye " + firstName);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request!!");
+        }
+        catch (Exception exception) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        }
+    }
+
+
 
 }
--- src/test/java/com/projects/emailclone/ControllerTests/EmailControllerTest.java
+++ src/test/java/com/projects/emailclone/ControllerTests/EmailControllerTest.java
@@ -4,13 +4,19 @@ import com.projects.emailclone.Controller.EmailController;
 import com.projects.emailclone.Model.User;
 import com.projects.emailclone.Service.UserImplementation;
 import com.projects.emailclone.Service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.hibernate.Session;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -20,11 +26,16 @@ public class EmailControllerTest {
     private UserService userService;
     @Mock
     private UserImplementation userImplementation;
+    @Mock
+    private HttpSession httpSession;
+    @Mock
+    private HttpServletRequest request;
     @InjectMocks
     private EmailController emailController;
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        Mockito.when(request.getSession(true)).thenReturn(httpSession);
     }
 
     @Test
@@ -50,29 +61,54 @@ public class EmailControllerTest {
     @Test
     void userLogin_Success() {
         User user = new User();
+        String result = "1) Show Messages\n2) Send Messages\n3) Log-out";
         when(userImplementation.checkIfUserExists(user.getFirstName())).thenReturn(true);
         when(userImplementation.checkPassword(user.getFirstName(), user.getPassword())).thenReturn(true);
-        ResponseEntity<String> response = emailController.loginUser(user.getFirstName(), user.getPassword());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(response.getBody(), "Login Successfully");
+        String response = emailController.loginUser(user.getFirstName(), user.getPassword(), request);
+        assertEquals(result, response);
     }
 
     @Test
     void userLogin_User_NotFound() {
         User user = new User();
+        String result = "User not found";
         when(userImplementation.checkIfUserExists(user.getFirstName())).thenReturn(false);
-        ResponseEntity<String> response = emailController.loginUser(user.getFirstName(), user.getPassword());
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals(response.getBody(), "User Not found");
+        String response = emailController.loginUser(user.getFirstName(), user.getPassword(), request);
+        assertEquals(result, response);
     }
 
     @Test
     void userLogin_User_InCorrect_Password() {
         User user = new User();
+        String result = "Incorrect password!!";
         when(userImplementation.checkIfUserExists(user.getFirstName())).thenReturn(true);
         when(userImplementation.checkPassword(user.getFirstName(), user.getPassword())).thenReturn(false);
-        ResponseEntity<String> response = emailController.loginUser(user.getFirstName(), user.getPassword());
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals(response.getBody(), "Incorrect Password");
+        String response = emailController.loginUser(user.getFirstName(), user.getPassword(), request);
+        assertEquals(result, response);
     }
+
+    @Test
+    void directToSelectedOption() {
+        HttpSession session = mock(HttpSession.class);
+        when(session.isNew()).thenReturn(false);
+        try {
+            final Field field = EmailController.class.getDeclaredField("userSession");
+            field.setAccessible(true);
+            field.set(emailController, session);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        ResponseEntity<String> response1 = emailController.directToSelectedOption(1);
+        assertEquals(HttpStatus.OK, response1.getStatusCode());
+        assertEquals("Show Messages", response1.getBody());
+
+        ResponseEntity<String> response2 = emailController.directToSelectedOption(2);
+        assertEquals(HttpStatus.OK, response2.getStatusCode());
+        assertEquals("Send Messages", response2.getBody());
+
+        ResponseEntity<String> response3 = emailController.directToSelectedOption(3);
+        assertEquals(HttpStatus.OK, response3.getStatusCode());
+        assertEquals("Log out", response3.getBody());
+    }
+
 }
